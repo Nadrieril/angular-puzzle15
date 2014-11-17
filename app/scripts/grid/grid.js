e2b4668efdbea3e8c191dfe0bf094ae558987819
Nadrieril <nadrieril@gmail.com>
2014-11-17 02:18:48 +0100

    Inverted commands

--- app/scripts/grid/grid.js
+++ app/scripts/grid/grid.js
@@ -87,10 +87,10 @@ angular.module('Grid', [])
 
         // Private things
         var vectors = {
-            'left': { x: -1, y: 0 },
-            'right': { x: 1, y: 0 },
-            'up': { x: 0, y: -1 },
-            'down': { x: 0, y: 1 }
+            'left': { x: 1, y: 0 },
+            'right': { x: -1, y: 0 },
+            'up': { x: 0, y: 1 },
+            'down': { x: 0, y: -1 }
         };
 
         this.getSize = function() {
